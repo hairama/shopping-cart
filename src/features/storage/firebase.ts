91abff8ab554ddf3528f3e6f2738400e423347f6
hairama <stoicform@pm.me>
2024-11-19 23:23:32 -0800

    update databaseURL

--- src/features/storage/firebase.ts
+++ src/features/storage/firebase.ts
@@ -6,6 +6,7 @@ const appSettings = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  dataseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
